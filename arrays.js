848a483d9aded5ef0aa4a264364cf33e72d8b9f4
Facundo Peralta <facundoperaltaezequiel@gmail.com>
2024-09-26 18:47:35 -0300

    cambio.

---
+++ arrays.js
@@ -0,0 +1,28 @@
+/* const array = ["Facundo", "Ezequiel","Peralta"];
+console.log(array); 
+ */
+
+class MyArray {
+    constructor (){
+        this.length = 0;
+        this.data = {}
+        
+
+    }
+    get(index){
+        return this.data[index];
+    }
+    push(item){
+        this.data[this.length] = item;
+        this.length++;
+        return this.data;
+    }   
+    pop(){
+        const lastItem = this.data[this.length-1];
+        delete this.data[this.length-1];
+        this.length--;
+        return lastItem;
+    }
+    
+}
+const arrayMio = new MyArray();
