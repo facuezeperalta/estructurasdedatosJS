7327a74d982d38ad6f256c130bb53342c3853de9
Facundo Peralta <facundoperaltaezequiel@gmail.com>
2024-09-26 19:47:44 -0300

    array terminado.

--- arrays.js
+++ arrays.js
@@ -19,10 +19,21 @@ class MyArray {
     }   
     pop(){
         const lastItem = this.data[this.length-1];
-        delete this.data[this.length-1];
+        delete this.data[this.length-1]; //delete es la palabra reservada en JS para borrar elementos.
         this.length--;
-        return lastItem;
+        return lastItem; //el método pop regresa el elemento que fue eliminado. Lo traemos desde lastItem.
     }
-    
+    myDelete(index){
+        const item = this.data[index];
+        this.shiftIndex(index); //con el método shift index nos aseguramos que todos los elementos se re acomoden en el array.
+        return item;
+    }
+    shiftIndex(index) {
+        for(let i = index; i < this.length-1;i++){
+            this.data[i] = this.data[i+1];
+        } 
+        delete this.data[this.length -1]; //aca borramos el item
+        this.length--; // borramos el elementos y bajamos el tamaño del array.
+    }
 }
 const arrayMio = new MyArray();
