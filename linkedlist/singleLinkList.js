2650c977f9e894d9841e227f327c4e2c4ead7bc9
Facundo Peralta <facundoperaltaezequiel@gmail.com>
2024-10-23 16:47:17 -0300

    Linked list y agregar nodos al final y al comienzo.

---
+++ linkedlist/singleLinkList.js
@@ -0,0 +1,54 @@
+/* 
+let singleLinkList = {
+    head: {
+        value : 1,
+        next : {
+            value: 2,
+            next:{
+                value:3,
+                next: {
+                    value:4,
+                    next: null
+                }
+            }
+        }
+    }
+} */
+
+class Nodo{
+    constructor(value){
+        this.value = value;
+        this.next = null;
+    }
+}
+
+class MySingleLinkList{
+    constructor(value){
+        this.head = {
+            value: value,
+            next: null
+        }
+        this.tail = this.head; /* como es el primer valor apunta de nuevo a la cabeza */
+        this.length  = 1; /* obligamos que cuando se haga la instancia de la clase de agregue el primer nodo o sea lleva un elemento si o si  */
+    }
+    append(value){
+        const newNode = new Nodo(value);
+
+        this.tail.next = newNode; //agregamos el nodo a la cola
+        this.tail = newNode; //posicionamos el nuevo nodo en la cola.
+        this.length++;
+
+        return this
+    }
+    prepend(value){
+        const newNode = new Nodo(value);
+        newNode.next = this.head; //Hago que el nodo sea la cabeza
+        this.head = newNode; // asigno el nodo a la cabeza.
+        this.length++; //incremento el valor de la lista en 1.
+        return this;
+        
+    }
+}
+
+let mylinkListed = new MySingleLinkList(1); //instancia de la clase.
+
